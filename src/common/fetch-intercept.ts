1997bd5364cd34fcaadd66c35951edf387f23e85
Martín Ugarte <contact@martinugarte.com>
2021-10-18 14:10:04 -0300

    Update CSRF service in fetch-intercept

--- src/common/fetch-intercept.ts
+++ src/common/fetch-intercept.ts
@@ -1,12 +1,12 @@
 import fetchIntercept from 'fetch-intercept'
 import Cookies from 'js-cookie'
-import { Service } from '../lib/api/services/Service'
+import { CsrfService } from '../lib/api/services/CsrfService'
 
 const unregister = fetchIntercept.register({
   request: async function (url, config) {
     if ( ['POST', 'PUT', 'PATCH', 'DELETE'].includes(config.method || 'PUT') ) {
       if ( !Cookies.get('csrftoken') ) {
-        await Service.setCsrfCookie()
+        await CsrfService.setCsrfCookie()
       }
       const modifiedHeaders = new Headers(config.headers)
       modifiedHeaders.append('X-CSRFToken', Cookies.get('csrftoken') || '')
